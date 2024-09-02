572b6afacbd9be26ad7583511b8939738ea07e7c
aarav0180 <aarav.18o2005@gmail.com>
2024-09-03 01:30:09 +0530

    Login page completed

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:night_bite/Pages/Login.dart';
 
+import 'firebase_options.dart';
+
 
 
 void main() {
