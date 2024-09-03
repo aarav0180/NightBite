714cfa6df326cae150cb0610cfde4dbd09e3d278
aarav0180 <aarav.18o2005@gmail.com>
2024-09-03 14:44:39 +0530

    SignUp backend code written

--- lib/Pages/SignUp.dart
+++ lib/Pages/SignUp.dart
@@ -1,7 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:night_bite/Pages/Homepage.dart';
+import 'package:night_bite/Pages/Login.dart';
+import 'package:random_string/random_string.dart';
 
 import '../Widgets/service_widget.dart';
+import '../services/database.dart';
+import '../services/shared_pref.dart';
 
 class Signup extends StatefulWidget {
   const Signup({super.key});
@@ -11,6 +18,44 @@ class Signup extends StatefulWidget {
 }
 
 class _SignupState extends State<Signup> {
+
+
+  String? name, email, password;
+  TextEditingController nameController = new TextEditingController();
+  TextEditingController emailController = new TextEditingController();
+  TextEditingController passwordController = new TextEditingController();
+
+  final _formkey = GlobalKey<FormState>();
+
+  registration() async{
+    if (password!=null && name!=null && email!=null){
+      try{
+        UserCredential userCredential= await FirebaseAuth.instance.createUserWithEmailAndPassword(email: email!, password: password!);
+
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            backgroundColor: Colors.greenAccent,
+            content: Text("registered Successfully", style: TextStyle(color: Colors.black, fontSize: 16))));
+
+        Navigator.push(context,MaterialPageRoute(builder: (context) => Homepage()) );
+
+      }on FirebaseException catch(e) {
+        if(e.code=="weak"){
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              backgroundColor: Colors.greenAccent,
+              content: Text("password provided is too weak", style: TextStyle(color: Colors.black, fontSize: 16))));
+        }
+
+        else if(e.code=="email already in use"){
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              backgroundColor: Colors.greenAccent,
+              content: Text("account already exist", style: TextStyle(color: Colors.black, fontSize: 16))));
+
+        }
+      }
+
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,110 +77,132 @@ class _SignupState extends State<Signup> {
                 width: MediaQuery.of(context).size.width,
                 //height: MediaQuery.of(context).size.height/1.8,
                 decoration: const BoxDecoration(color: Color.fromRGBO(106,156,137,1), borderRadius: BorderRadius.only(topLeft: Radius.circular(41), topRight: Radius.circular(41))),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
+                child: Form(
+                  key: _formkey,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
 
-                    const SizedBox(height: 10,),
-                    const Center(child: const Text("Login", style: TextStyle(color: Colors.black, fontSize: 54, fontWeight: FontWeight.bold,))),
-                    const SizedBox(height: 30,),
+                      const SizedBox(height: 10,),
+                      const Center(child: const Text("Sign Up", style: TextStyle(color: Colors.black, fontSize: 54, fontWeight: FontWeight.bold,))),
+                      const SizedBox(height: 30,),
 
-                    //Text("Login", style: AppWidget.semiBoldTextStyle(),),
+                      //Text("Login", style: AppWidget.semiBoldTextStyle(),),
 
-                    Container(
-                      margin: const EdgeInsets.symmetric(horizontal: 20),
-                      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 2),
-                      decoration: BoxDecoration(color: const Color.fromRGBO(255,245,228,1), borderRadius: BorderRadius.circular(14)),
-                      child: TextFormField(
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please enter your email';
+                      Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 20),
+                        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 2),
+                        decoration: BoxDecoration(color: const Color.fromRGBO(255,245,228,1), borderRadius: BorderRadius.circular(14)),
+                        child: TextFormField(
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter your name';
+                            }
+                            return null;
+                          },
+                          controller: nameController,
+                          decoration: InputDecoration(border: InputBorder.none, hintText: "Name"),
+                        ),
+                      ),
+
+                      const SizedBox(height: 30,),
+
+                      Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 20),
+                        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 2),
+                        decoration: BoxDecoration(color: const Color.fromRGBO(255,245,228,1), borderRadius: BorderRadius.circular(14)),
+                        child: TextFormField(
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter your email';
+                            }
+                            return null;
+                          },
+                          controller: emailController,
+                          decoration: InputDecoration(border: InputBorder.none, hintText: "Email"),
+                        ),
+                      ),
+
+                      const SizedBox(height: 20,),
+
+                      Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 20),
+                        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 2),
+                        decoration: BoxDecoration(color: const Color.fromRGBO(255,245,228,1), borderRadius: BorderRadius.circular(14)),
+                        child: TextFormField(
+                          obscureText: true,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter your password';
+                            }
+                            return null;
+                          },
+                          controller: passwordController,
+                          decoration: InputDecoration(border: InputBorder.none, hintText: "Password"),
+                        ),
+                      ),
+
+                      const SizedBox(height: 20,),
+
+                      Container(
+                        margin: const EdgeInsets.symmetric(horizontal: 20),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              padding:const EdgeInsets.symmetric(horizontal: 23, vertical: 10),
+                              decoration: BoxDecoration(color: const Color.fromRGBO(193,216,195,1), borderRadius: BorderRadius.circular(15)),
+                              child: Row(
+                                children: [
+
+                                  SvgPicture.asset('image/google.svg', height: 30, width: 30,),
+                                  SizedBox(width: 9,),
+                                  Text("Google", style: TextStyle(color: Colors.black, fontSize: 18),),
+                                ],),
+                            ),
+
+                            SizedBox(width: 40,),
+                            //Text("Don't have an account ?", style: AppWidget.lightTextStyle(),),
+                            Container(
+                              padding:const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                              decoration: BoxDecoration(color:const Color.fromRGBO(193,216,195,1), borderRadius: BorderRadius.circular(15) ),
+                              child: GestureDetector(
+                                  onTap: (){
+                                    Navigator.push(context,MaterialPageRoute(builder: (context) => Login()) );
+                                  },
+                                  child: const Text("Existing User ?", style: TextStyle(color: Colors.black, fontWeight: FontWeight.w500, fontSize: 18),)),
+                            )
+                          ],
+                        ),
+                      ),
+
+                      const SizedBox(height: 30,),
+
+                      GestureDetector(
+                        onTap: () {
+                          if(_formkey.currentState!.validate()){
+                            setState(() {
+                              name = nameController.text;
+                              email = emailController.text;
+                              password=passwordController.text;
+                              registration();
+                            });
                           }
-                          return null;
                         },
-                        //controller: emailController,
-                        decoration: InputDecoration(border: InputBorder.none, hintText: "Email"),
+                        child: Container(
+                          width: MediaQuery.of(context).size.width,
+                          margin: const EdgeInsets.symmetric(horizontal: 20, ),
+                          padding: const EdgeInsets.symmetric(vertical: 10),
+                          decoration: BoxDecoration(color: const Color.fromRGBO(205,92,8,1), borderRadius: BorderRadius.circular(14)),
+                          child: Center(child: Text("Sign Up", style: AppWidget.semiBoldTextStyle(),)),
+                        ),
                       ),
-                    ),
 
-                    const SizedBox(height: 30,),
+                      SizedBox(height: 30,),
 
-                    Container(
-                      margin: const EdgeInsets.symmetric(horizontal: 20),
-                      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 2),
-                      decoration: BoxDecoration(color: const Color.fromRGBO(255,245,228,1), borderRadius: BorderRadius.circular(14)),
-                      child: TextFormField(
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please enter your password';
-                          }
-                          return null;
-                        },
-                        //controller: emailController,
-                        decoration: InputDecoration(border: InputBorder.none, hintText: "Password"),
-                      ),
-                    ),
-
-                    const SizedBox(height: 20,),
-
-                    Container(
-                      margin:const EdgeInsets.symmetric(horizontal: 20),
-                      child: const Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          Text("Forget Password ?", style: TextStyle(color: Colors.black, fontSize: 18, fontWeight: FontWeight.w500)),
-                        ],
-                      ),
-                    ),
-
-                    const SizedBox(height: 20,),
-
-                    Container(
-                      margin: const EdgeInsets.symmetric(horizontal: 20),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Container(
-                            padding:const EdgeInsets.symmetric(horizontal: 23, vertical: 10),
-                            decoration: BoxDecoration(color: const Color.fromRGBO(193,216,195,1), borderRadius: BorderRadius.circular(15)),
-                            child: Row(
-                              children: [
-
-                                SvgPicture.asset('image/google.svg', height: 30, width: 30,),
-                                SizedBox(width: 9,),
-                                Text("Google", style: TextStyle(color: Colors.black, fontSize: 18),),
-                              ],),
-                          ),
-
-                          SizedBox(width: 40,),
-                          //Text("Don't have an account ?", style: AppWidget.lightTextStyle(),),
-                          Container(
-                            padding:const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
-                            decoration: BoxDecoration(color:const Color.fromRGBO(193,216,195,1), borderRadius: BorderRadius.circular(15) ),
-                            child: GestureDetector(
-                                onTap: (){
-                                  Navigator.push(context,MaterialPageRoute(builder: (context) => Signup()) );
-                                },
-                                child: const Text("New User ?", style: TextStyle(color: Colors.black, fontWeight: FontWeight.w500, fontSize: 18),)),
-                          )
-                        ],
-                      ),
-                    ),
-
-                    const SizedBox(height: 30,),
-
-                    Container(
-                      width: MediaQuery.of(context).size.width,
-                      margin: const EdgeInsets.symmetric(horizontal: 20, ),
-                      padding: const EdgeInsets.symmetric(vertical: 10),
-                      decoration: BoxDecoration(color: const Color.fromRGBO(205,92,8,1), borderRadius: BorderRadius.circular(14)),
-                      child: Center(child: Text("Login", style: AppWidget.semiBoldTextStyle(),)),
-                    ),
-
-                    SizedBox(height: 30,),
-
-                  ],
+                    ],
+                  ),
                 ),
               )
             ],
---
+++ lib/services/shared_pref.dart
@@ -0,0 +1,40 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class SharedPreferenceHelper {
+
+  static String userIdKey = "USERKEY";
+  static String userNameKey = "USERNAMEKEY";
+  static String userEmailKey = "USEREMAILKEY";
+  //static String userImageKey = "USERIMAGEKEY";
+
+  Future<bool> saveUserId(String getUserId) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.setString(userIdKey, getUserId);
+  }
+
+  Future<bool> saveUserName(String getUserName) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.setString(userNameKey, getUserName);
+  }
+
+  Future<bool> saveUserEmail(String getUserEmail) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.setString(userEmailKey, getUserEmail);
+  }
+
+  Future<String?> getUserId() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getString(userIdKey);
+  }
+
+  Future<String?> getUserName() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getString(userNameKey);
+  }
+
+  Future<String?> getUserEmail() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getString(userEmailKey);
+  }
+
+}
